4802032edaff71f106b11364f1627f9f32fa2bf5
Hneumont <hberumengovea@student.neumont.edu>
2026-03-05 09:03:53 -0700

    Revert "Update app.js"
    This reverts commit 5393add98e57b498c61d007f5b5677a49d6b860f.

--- public/app.js
+++ public/app.js
@@ -47,12 +47,8 @@ signupBtn.addEventListener("click", () => {
 let cvReady = false;
 let useFilter = true;
 
-
 let stream = null;
 let rafId = null;
-// load pre-trained classifiers
-classifier.load('haarcascade_frontalface_default.xml');
-
 
 function setStatus(text) {
   statusEl.textContent = text;
@@ -190,25 +186,12 @@ function startProcessingLoop() {
 
     const src = cv.imread(canvas);
     const dst = new cv.Mat();
-    let gray = new cv.Mat();
-    let cap = new cv.VideoCapture(video);
-    let faces = new cv.RectVector();
-    let classifier = new cv.CascadeClassifier();
 
     try {
-      cap.read(src);
-      src.copyTo(dst);
-      cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
-      // detect faces.
-      classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
-        // draw faces.
-      for (let i = 0; i < faces.size(); ++i) {
-          let face = faces.get(i);
-          let point1 = new cv.Point(face.x, face.y);
-          let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-          cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
-      }
-      cv.imshow('canvasOutput', dst);
+      cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+      cv.Canny(dst, dst, 60, 120);
+      cv.cvtColor(dst, dst, cv.COLOR_GRAY2RGBA);
+      cv.imshow(canvas, dst);
     } finally {
       src.delete();
       dst.delete();
